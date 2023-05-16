c0c81f693827648712edd12e889a980ddafe2f8f
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-05-16 11:58:02 -0300

    ,

--- backend/routes/router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-
-const userRouter = require('./user')
-const loginRouter = require('./login')
-
-router.use('/', userRouter, loginRouter)
-
-
-module.exports = router
